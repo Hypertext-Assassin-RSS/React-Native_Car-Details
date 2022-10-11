4589d2c39c4fe5d0dc8db76a65669415980adbf7
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2022-10-11 11:58:49 +0530

    Signup screen and Signup to Signin navigation

--- screens/Signup.js
+++ screens/Signup.js
@@ -1,10 +1,56 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
+import { Box, Button, Center, FormControl, Heading, HStack, Input, Link, NativeBaseProvider, Text, VStack } from 'native-base'
 
-export default function Signup() {
+
+export default function Signup({navigation}) {
   return (
-    <View>
-      <Text>Sign up</Text>
-    </View>
+    <NativeBaseProvider>
+      <Center w="100%">
+      <Box safeArea p="2" w="90%" maxW="290" py="8">
+        <Heading size="lg" color="coolGray.800" _dark={{
+        color: "warmGray.50"
+      }} fontWeight="semibold">
+          Welcome
+        </Heading>
+        <Heading mt="1" color="coolGray.600" _dark={{
+        color: "warmGray.200"
+      }} fontWeight="medium" size="xs">
+          Sign up to continue!
+        </Heading>
+        <VStack space={3} mt="5">
+          <FormControl>
+            <FormControl.Label>Email</FormControl.Label>
+            <Input />
+          </FormControl>
+          <FormControl>
+            <FormControl.Label>Password</FormControl.Label>
+            <Input type="password" />
+          </FormControl>
+          <FormControl>
+            <FormControl.Label>Confirm Password</FormControl.Label>
+            <Input type="password" />
+          </FormControl>
+          <Button mt="2" colorScheme="indigo">
+            Sign up
+          </Button>
+          <HStack mt="6" justifyContent="center">
+            <Text fontSize="sm" color="coolGray.600" _dark={{
+            color: "warmGray.200"
+          }}>
+              I'm a new user.{" "}
+            </Text>
+            <Link _text={{
+            color: "indigo.500",
+            fontWeight: "medium",
+            fontSize: "sm"
+          }}  onPress={()=>{navigation.navigate('Sign in')}}>
+              Sign In
+            </Link>
+          </HStack>
+        </VStack>
+      </Box>
+    </Center>
+    </NativeBaseProvider>
   )
 }
