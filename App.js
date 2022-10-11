912d5efb2c62b898ad4272aa49f87d5f2d86315c
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2022-10-11 11:46:54 +0530

    Sign in screen

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import Login from './screens/Login'
 import { createStackNavigator } from '@react-navigation/stack';
 import Register from './screens/Register'
+import Signin from './screens/Signin';
 
 
 const Stack = createStackNavigator();
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Sign in" component={Signin} />
       <Stack.Screen name="Register" component={Register} />
     </Stack.Navigator>
     </NavigationContainer>
