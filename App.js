9df0803ba67b8a6b8de3b62e475fa25aca29c7a5
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2022-10-13 16:12:28 +0530

    add image picker to post screen

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack';
+import {View, Text} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import Signin from './screens/Signin';
 import Signup from './screens/Signup';
 import Post from './screens/Post';
-
 
 const Stack = createStackNavigator();
 
@@ -13,10 +12,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen name="Sign in" component={Signin} />
-      <Stack.Screen name="Sign up" component={Signup} />
-      <Stack.Screen name='Post' component={Post} />
-    </Stack.Navigator>
+        <Stack.Screen name="Sign in" component={Signin} />
+        <Stack.Screen name="Post" component={Post} />
+        <Stack.Screen name="Sign up" component={Signup} />
+      </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
--- screens/Post.js
+++ screens/Post.js
@@ -1,12 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, {useState, useCallback} from 'react';
+import {StyleSheet, View} from 'react-native';
+import * as ImagePicker from 'react-native-image-picker';
 
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import {ImagePickerModal} from '../components/image-picker-modal';
+import {ImagePickerAvatar} from '../components/image-picker-avatar';
 
 export default function Post() {
+  const [pickerResponse, setPickerResponse] = useState(null);
+  const [visible, setVisible] = useState(false);
+
+  const onImageLibraryPress = useCallback(() => {
+    const options = {
+      selectionLimit: 1,
+      mediaType: 'photo',
+      includeBase64: false,
+    };
+    ImagePicker.launchImageLibrary(options, setPickerResponse);
+  }, []);
+
+  const onCameraPress = React.useCallback(() => {
+    const options = {
+      saveToPhotos: true,
+      mediaType: 'photo',
+      includeBase64: false,
+    };
+    ImagePicker.launchCamera(options, setPickerResponse);
+  }, []);
+
+  const uri = pickerResponse?.assets && pickerResponse.assets[0].uri;
+
   return (
-    <View>
-      <Text>post</Text>
+    <View style={styles.screen}>
+      <ImagePickerAvatar uri={uri} onPress={() => setVisible(true)} />
+      <ImagePickerModal
+        isVisible={visible}
+        onClose={() => setVisible(false)}
+        onImageLibraryPress={onImageLibraryPress}
+        onCameraPress={onCameraPress}
+      />
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: '#f2f2fC',
+  },
+});
